b202e009d3331fa872ba28206b1fde3b9da99d77
Illia <illia.kovalc@gmail.com>
2017-06-25 18:11:52 +0300

    readme + webpackdev server

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Poster-POS-Platfrom-Bolerplate
+
+1
+<kbd> npm install</kbd> 
+
+2
+<kbd> npm install webpack-dev-server -g</kbd> 
+
+3
+<kbd> webpack-dev-server --port 1511</kbd>
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+'use strict';
+var path = require("path");
+
+module.exports = {
+	context: __dirname,
+	entry: {
+		'bundle.js': "./src/app.js"
+	},
+
+	output: {
+		path: path.resolve(__dirname, './'),
+		filename: "[name]"
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				loader: 'babel-loader?cacheDirectory',
+				exclude: [
+				],
+				include: [
+					__dirname + '/src'
+				],
+
+				query: {
+					presets: ['es2015', 'stage-0']
+				}
+			}
+		]
+	},
+
+	devtool: 'source-map',
+
+	watch: true,
+
+	watchOptions: {
+		aggregateTimeout: 100
+	},
+
+	plugins: [
+	]
+};
+
+
